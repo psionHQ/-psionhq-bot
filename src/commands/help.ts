cc85cfd610d95e2f0daaf0552b1cb1002cda84d8
Ψ <psionhq@gmail.com>
2026-07-24 15:00:23 +0300

    Update help.ts
    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,1 +1,11 @@
-help.ts
+import { bot } from "../bot";
+
+bot.command("help", (ctx) => {
+  ctx.reply(`
+📖 Доступные команды:
+
+/start — начало работы
+/help — помощь
+/about — информация о проекте PSI
+  `);
+});
