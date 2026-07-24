0e0c9098d35d3c8e48d309a9e756f7db8eef410e
Ψ <psionhq@gmail.com>
2026-07-24 14:55:16 +0300

    Update start.ts
    Update start.ts

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,1 +1,18 @@
-start.ts
+import { bot } from "../bot";
+
+bot.start((ctx) => {
+  ctx.reply(`
+👋 Добро пожаловать в PSI Network!
+
+Это официальный бот проекта @PsionHQ.
+
+🚀 Скоро здесь появятся:
+• Новости проекта
+• Информация о токене PSI
+• Whitepaper
+• Roadmap
+• Сообщество
+
+Добро пожаловать!
+`);
+});
