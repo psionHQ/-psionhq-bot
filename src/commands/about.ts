dfb8aa041529747868f632d1d7fd9ef8e6aa03c4
Ψ <psionhq@gmail.com>
2026-07-24 15:01:33 +0300

    Update about.ts
    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,1 +1,14 @@
-about.ts
+import { bot } from "../bot";
+
+bot.command("about", (ctx) => {
+  ctx.reply(`
+🧠 PSI Network
+
+⚛️ Quantum Blockchain
+🔐 Secure Infrastructure
+🌍 Decentralized Ecosystem
+
+Official Bot:
+@PsionHQ_bot
+  `);
+});
